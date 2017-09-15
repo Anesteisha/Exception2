7a75b3164e364da11dee8a019ee9ec154edf70f5
Steisy <alena.burakovskaya@gmail.com>
2017-09-16 00:10:45 +0200

    Гласные и согласные

---
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0923/Solution.java
@@ -0,0 +1,54 @@
+package com.javarush.task.task09.task0923;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Гласные и согласные
+Написать программу, которая вводит с клавиатуры строку текста.
+Программа должна вывести на экран две строки:
+1. первая строка содержит только гласные буквы из введённой строки.
+2. вторая — только согласные буквы и знаки препинания из введённой строки.
+Буквы соединять пробелом, каждая строка должна заканчиваться пробелом.
+Пример ввода:
+Мама мыла раму.
+
+Пример вывода:
+а а ы а а у
+М м м л р м .
+*/
+
+public class Solution {
+    public static char[] vowels = new char[]{'а', 'я', 'у', 'ю', 'и', 'ы', 'э', 'е', 'о', 'ё'};
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String st = reader.readLine();
+        String gl = "";
+        String sogl = "";
+        for (int i = 0; i <st.length(); i++) {
+            char a = st.charAt(i);
+            if (isVowel(a)) {
+                gl = gl+a + " ";
+            } else if (a==' ') {}
+            else
+                {
+                sogl =sogl+ a + " ";
+            }
+        }
+        System.out.println(gl);
+        System.out.println(sogl);
+    }
+
+    // метод проверяет, гласная ли буква
+    public static boolean isVowel(char c) {
+        c = Character.toLowerCase(c);  // приводим символ в нижний регистр - от заглавных к строчным буквам
+
+        for (char d : vowels)   // ищем среди массива гласных
+        {
+            if (c == d)
+                return true;
+        }
+        return false;
+    }
+}
